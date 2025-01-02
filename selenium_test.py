7c2cda5684b01e5809b2886763c1ed8c5e8ad546
szc-aa <646099838@qq.com>
2025-01-02 14:47:28 +0800

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+"""assssasas"""
 
 baidu_search = {
     'button_search':'//input[@value = "百度一下"]',
